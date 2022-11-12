016e32612ced07fbfeafea6ac2c74fb8d0f48ab8
666Parzival666 <oven.igor@yandex.ru>
2022-11-12 20:02:02 +0300

    HomeWork_5

--- JS/4.7.js
+++ JS/4.7.js
@@ -1,31 +1,34 @@
 function cesar(action, shift, text){
     alphabet = ['а','б','в','г','д','е','ё','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч',',ш','щ','ъ','ы','ь','э','ю','я']
     mess = ''
-    if( action == 'encode'){
-        for(let i = 0; i > text.lenght; i++){
-            if(text[i] in alphabet){
+    shift = Number(shift)
+    if(action == 'decode'){
+        for(let i = 0; i < text.length; i++){
                 letter_id = alphabet.indexOf(text[i])
-                if(letter_id + shift > alphabet.lenght){
-                    mess += alphabet[((letter_id + shift) - alphabet.lenght) - 1]
+                if((letter_id + shift) >= alphabet.length){
+                    mess = mess + (alphabet[(letter_id + shift) - alphabet.length])
                 }
-                else{                
-                    mess += alphabet[letter_id + shift]
+                else{    
+                    mess = mess + (alphabet[letter_id + shift])
                 }
-            }
+            
         }
     }
-    else if( action == 'decode'){
-        for(let i = 0; i > text.lenght; i++){
-            if(text[i] in alphabet){
+    else if(action == 'encode'){
+        for(let i = 0; i < text.length; i++){
                 letter_id = alphabet.indexOf(text[i])
-                if(letter_id + shift < alphabet.lenght){
-                    mess += alphabet[(alphabet.lenght + (letter_id - shift)) + 1]
+                if((letter_id - shift) < 0){
+                    mess = mess + (alphabet[(letter_id - shift) + alphabet.length])
                 }
-                else{                
-                    mess += alphabet[letter_id - shift]
+                else{    
+                    mess = mess + (alphabet[letter_id - shift])
                 }
-            }
+            
         }
-     } 
+    }
      return mess
 }
+action = prompt("Вы хотите encode или decode?", ' ')
+shift = prompt("Какой сдвиг?", ' ')
+text = prompt("Введите текст", ' ')
+alert(cesar(action, shift, text))
